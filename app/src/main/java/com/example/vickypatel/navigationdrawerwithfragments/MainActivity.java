66b8cfad49f7a99d702fefb129935caee94aa7d0
vicky patel <vicky.patel@hotmail.com>
2017-03-06 15:51:38 -0500

    initial commit

--- app/src/main/java/com/example/vickypatel/navigationdrawerwithfragments/MainActivity.java
+++ app/src/main/java/com/example/vickypatel/navigationdrawerwithfragments/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.vickypatel.navigationdrawerwithfragments;
 
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
     }
 
 
