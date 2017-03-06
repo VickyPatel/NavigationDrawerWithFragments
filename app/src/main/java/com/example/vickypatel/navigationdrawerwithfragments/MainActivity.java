af9c5be1ff19e1ee192d647a90e8d83dc0825281
vicky patel <vicky.patel@hotmail.com>
2017-03-06 16:42:19 -0500

    menu added

--- app/src/main/java/com/example/vickypatel/navigationdrawerwithfragments/MainActivity.java
+++ app/src/main/java/com/example/vickypatel/navigationdrawerwithfragments/MainActivity.java
@@ -1,12 +1,20 @@
 package com.example.vickypatel.navigationdrawerwithfragments;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    Toast mToast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +30,46 @@ public class MainActivity extends AppCompatActivity {
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        if(mToast != null){
+            mToast.cancel();
+        }
+        switch (item.getItemId()) {
+            case R.id.home:
+                mToast = Toast.makeText(this, "Home", Toast.LENGTH_LONG);
+                break;
+            case R.id.artists:
+                mToast= Toast.makeText(this, "Artists", Toast.LENGTH_LONG);
+                break;
+            case R.id.feedback:
+                mToast = Toast.makeText(this, "Feedback", Toast.LENGTH_LONG);
+                break;
+            case R.id.contact_us:
+                mToast = Toast.makeText(this, "Contact us", Toast.LENGTH_LONG);
+                break;
+            default:
+                break;
+        }
+        mToast.show();
+        DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
 }
